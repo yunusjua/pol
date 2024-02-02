a8eca5681b6314929e479fb2b11c2be9b381e819
yunusjua <143286627+yunusjua@users.noreply.github.com>
2024-02-02 16:23:21 +0700

    Delete start_unmineable.sh

--- start_unmineable.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cpu_threads=$(grep -c '^processor' /proc/cpuinfo)
-echo "Start XMRig with Parameters: -a $ALGO -o $POOL_ADDRESS -u LTC:$WALLET_USER.$HOSTNAME#Jumper -p $PASSWORD --threads=$cpu_threads"
